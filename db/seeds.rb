67a48a109c7c12b77858d957b4fdea107700e85f
nchhomtythya <nctythya@outlook.com>
2022-12-02 20:29:33 +1100

    version-1-car-seed-file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database"
+Car.destroy_all
+
+puts "Creating cars"
+
+20.times do
+  car = Car.create(
+    model: Faker::Vehicle.model,
+    make: Faker::Vehicle.make,
+    year: Faker::Vehicle.year,
+    category: Faker::Vehicle.car_type,
+    description: Faker::Vehicle.standard_specs,
+    price: rand(20000..30000),
+    availability: true,
+    user_id: 1,
+    transmission: Faker::Vehicle.drive_type,
+    colour: Faker::Vehicle.color
+  )
+  puts "Car with model: #{car.model} has been created!"
+end
+
+puts "Finished!"
