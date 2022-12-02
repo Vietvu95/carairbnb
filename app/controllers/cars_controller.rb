525c1165dcd04ea3bec2fde1424bc6ba6cda96bd
nchhomtythya <nctythya@outlook.com>
2022-12-03 03:27:34 +1100

    car details page -show

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,10 +2,12 @@ class CarsController < ApplicationController
 
   before_action :set_car, only: [:show, :edit, :update, :destroy]
   def index
+    @car_images = ["car1.jpeg", "car2.jpeg", "car3.jpeg", "car4.jpeg", "car5.jpeg","car6.jpeg", "car7.webp", "car8.jpeg","car9.jpeg","car9.jpeg","car10.jpeg","car11.jpeg", "car12.jpeg", "car13.jpeg", "car14.webp","car15.jpeg"]
     @cars = Car.all
   end
 
   def show
+    @car_image = ["car1.jpeg", "car2.jpeg", "car3.jpeg", "car4.jpeg", "car5.jpeg","car6.jpeg", "car7.webp", "car8.jpeg","car9.jpeg","car9.jpeg","car10.jpeg","car11.jpeg", "car12.jpeg", "car13.jpeg", "car14.webp","car15.jpeg"].sample
   end
 
   def new
@@ -16,10 +18,11 @@ class CarsController < ApplicationController
     @car = Car.new(car_params)
     @car.user = current_user
     if @car.save
-    redirect_to cars_path
+      redirect_to new_car_path
     else
       render :new, status: :unprocessable_entity
     end
+
   end
 
 private
@@ -29,7 +32,7 @@ private
   end
 
   def car_params
-    params.require(:car).permit(:model, :make, :year, :category, :description, :price, :tranmission, :color)
+    params.require(:car).permit(:model, :make, :year, :category, :description, :price, :transmission, :colour, :availability)
   end
 
 end
