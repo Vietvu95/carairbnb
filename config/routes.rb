6b696c2c8f915b3abb404825ebadcf22a8869d41
nchhomtythya <nctythya@outlook.com>
2022-12-03 11:28:36 +1100

    update version of car model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :cars do
-    resources :bookings, only: %i[new]
+    resources :bookings, only: %i[new create destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
