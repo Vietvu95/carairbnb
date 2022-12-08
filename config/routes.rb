ff62bb5263c8a69172e0f77d629bd1a2a3a1ceea
nchhomtythya <nctythya@outlook.com>
2022-12-08 22:34:13 +1100

    Merge branch 'master' of github.com:Vietvu95/carairbnb into pudit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   end
   resources :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    resources :bookings, except: %i[new create]
   # Defines the root path route ("/")
   # root "articles#index"
 
