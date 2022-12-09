a8ca5f21ff862d22e1068757bf8413397bf16f05
chhomtythya norong <nctythya@yahoo.com>
2022-12-09 20:06:44 +1100

    links to navigate pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :cars do
-    resources :bookings, only: %i[index edit create edit update]
+    resources :bookings, only: %i[index new create edit update]
     resources :reviews, only: %i[new create]
   end
-  resources :bookings, :reviews, only: [:destroy]
+  resources :bookings, :reviews, only: %i[destroy show index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
