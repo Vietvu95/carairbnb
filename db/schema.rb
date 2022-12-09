993d5cc4ce476e6db70e1d359586529df89e28af
chhomtythya norong <nctythya@yahoo.com>
2022-12-09 11:02:57 +1100

    completed review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_095927) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_213211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,11 +75,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_095927) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.bigint "booking_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.bigint "car_id", null: false
+    t.text "content"
+    t.index ["car_id"], name: "index_reviews_on_car_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -104,6 +105,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_095927) do
   add_foreign_key "bookings", "cars"
   add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
-  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "cars"
   add_foreign_key "reviews", "users"
 end
