2d07406aefd80a2c5cc7a9c212144185a7b1a685
nchhomtythya <nctythya@outlook.com>
2022-12-10 12:37:40 +1100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  get "about", to: "dashboard#show"
+  get "dashboard", to: "dashboard#dashboard"
 end
